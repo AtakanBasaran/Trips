df2e970728008b4084b6f9a73c4dec77aaaeceb2
Atakan Başaran <basaranatakan10@gmail.com>
2024-07-01 15:17:20 +0300

    Initial Commit

---
+++ Trips/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Trips
+//
+//  Created by Atakan Başaran on 1.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Trips/TripsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripsApp.swift
+//  Trips
+//
+//  Created by Atakan Başaran on 1.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct TripsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
